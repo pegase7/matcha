f47c4e69df65eebf4e36a13d3e36ec5c33079855
pegase7 <75859305+pegase7@users.noreply.github.com>
2021-03-22 12:52:18 +0100

    Add files via upload

--- matcha.py
+++ matcha.py
@@ -4,6 +4,7 @@ import os
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+import time
 import psycopg2
 from UserManager import USERS_MANAGER
 from datetime import datetime
@@ -16,11 +17,14 @@ app.debug = True # a supprimer en production
 
 def send(unique, nature):
     lien = 'http://127.0.0.1:5000/validation/'+unique
+    f_time = time.asctime(time.localtime(time.time())).split()
+    
     Fromadd = "matcha@ik.me"
     Toadd = session['user']['email']   ##  Spécification du destinataire
     message = MIMEMultipart()    
     message['From'] = Fromadd   
-    message['To'] = Toadd   
+    message['To'] = Toadd 
+    message['Date'] =  f_time[0]+', '+f_time[2]+' '+f_time[1]+' '+f_time[4]+' '+f_time[3]+' +0100' +'\r\n'
     if nature == 'registration':
         message['Subject'] = "inscription"
         msg = "Bomjour, " + session['user']['name'] +" merci de valider votre inscription  en cliquant sur  ce lien " + lien 
@@ -31,15 +35,22 @@ def send(unique, nature):
     serveur = smtplib.SMTP('mail.infomaniak.com', 587)  ## Connexion au serveur sortant 
     serveur.starttls()    ## Spécification de la sécurisation
     serveur.login(Fromadd, "42Flask@lyon")    ## Authentification
-    texte= message.as_string().encode('utf-8')    
+    texte= message.as_string().encode('utf-8')  
     Toadds = [Toadd]
     serveur.sendmail(Fromadd, Toadds, texte)    ## Envoi du mail
     serveur.quit() 
 
 def lien_unique():
     a=''
-    for i in range(17):
-        a=a+chr((randint(34,127)))
+    for i in range(17): # compose une chaine aleatoire de lettres et de chiffres
+        ok=False
+        while ok==False:
+            x=randint(48,122)
+            if (x >=58 and x <=64) or (x>= 91 and x<=96):
+                ok = False
+            else:
+                ok = True
+        a=a+chr(x)
     return(a)
 
 @app.route('/',methods=['GET', 'POST'])
@@ -68,15 +79,22 @@ def homepage():
 def photo():
     if "user" not in session:
         return redirect(url_for('homepage')) 
+    ph1="/static/photo/"+session['user']['name']+"1.jpg"
+    ph2="/static/photo/"+session['user']['name']+"2.jpg"
+    ph3="/static/photo/"+session['user']['name']+"3.jpg"
+    ph4="/static/photo/"+session['user']['name']+"4.jpg"
+    ph5="/static/photo/"+session['user']['name']+"5.jpg"
     if request.method=="POST":
         f = request.files['maphoto']
         b=f.filename
+        num = request.form.get('numphoto')
+        photo_name = session['user']['name'] + num + '.jpg'
         path = 'static/photo'
-        f.save(os.path.join(path, 'temp.jpg'))
+        f.save(os.path.join(path, photo_name))
         
         return redirect(url_for('okphoto'))
     else:
-        return render_template('photo.html')
+        return render_template('photo.html',ph1 = ph1, ph2=ph2,ph3=ph3,ph4=ph4,ph5=ph5)
 
 @app.route('/accueil/')
 def accueil():
@@ -95,6 +113,7 @@ def profil():
         return redirect(url_for('homepage')) 
     users = USERS_MANAGER().get_users()
     user = session['user']['name']
+    ph1="/static/photo/"+session['user']['name']+"1.jpg"
     for u in users:
         if u[3] == user:
             nom = u[2]
@@ -107,7 +126,7 @@ def profil():
             b=str(u[11]) #champ date transformé en texte
             naissance=b[8:]+'/'+b[5:7]+'/'+b[:4] #conversion date americaine en europeene
 
-    return render_template('profil.html', nom = nom, prenom = prenom, sexe=sexe, orientation=orientation,bio=bio,email =email, naissance=naissance)
+    return render_template('profil.html',ph1=ph1, nom = nom, prenom = prenom, sexe=sexe, orientation=orientation,bio=bio,email =email, naissance=naissance)
 
 @app.route('/recherche/',methods=['GET', 'POST'])
 def recherche():
@@ -123,7 +142,7 @@ def registration():
         session['user']= {'name' : user, 'email' : mail}   
         lien=lien_unique()
         #ici enregistre le lien dans la fiche membre
-        send(lien,registration)
+        send(lien,'registration')
         return redirect(url_for('accueil'))
     else:
         return render_template('registration.html')
@@ -149,7 +168,7 @@ def forgot():
             lien=lien_unique()
             nature='password'
             #ici enregistre le lien dans la fiche membre
-            send(lien, nature)
+            send(lien, 'nature')
             return redirect(url_for('logout')) 
     return render_template('forgot_password.html',user=user)
 
@@ -169,8 +188,6 @@ def okphoto():
 def validation(code):
     return render_template('validation.html', code =code)
 
-@app.route('/reinit')
-def reinit():
-    user=session['user']['name']
-    #return render_template('forgot_password.html', user=user)
-    return redirect(url_for('forgot',user=user))
+@app.route('/newpassword/<code>', methods=['GET','POST'])
+def newpassword(code):
+    return render_template('newpassword.html', code =code)
