8002cfe1b5499621ea8f62423dd89bda7c4aa97e
Laurent Willer <lwiller@z1r4p5.42lyon.fr>
2021-03-29 14:43:18 +0200

    chat basic

--- matcho.py
+++ matcho.py
@@ -10,6 +10,7 @@ from UserManager import USERS_MANAGER # a supprimer a terme et progressivement
 from datetime import datetime
 from random import *
 from flask_socketio import SocketIO, join_room, send, emit, leave_room
+from time import localtime, strftime
 from hashage import *
 from matcha.orm.data_access import DataAccess
 from matcha.model.Users import Users
@@ -313,3 +314,44 @@ def add_header(r):
     r.headers["Expires"] = "0"
     r.headers['Cache-Control'] = 'public, max-age=0'
     return r
+
+
+# Temps réel avec socketio
+@socketio.on('message')
+def message(data):
+    # print(f"\n\n{data}\n\n")
+    send({'msg': data['msg'], 'username': data['username'], 
+    'time_stamp': strftime('%d-%b %I:%M%p', localtime())}, room=data['room'])
+    
+
+@socketio.on('join')
+def join(data):
+    join_room(data['room'])
+    send({'msg': data['username'] + " has join the " + data['room'] + " room."}, room=data['room'])
+
+
+@socketio.on('leave')
+def leave(data):
+    leave_room(data['room'])
+    send({'msg': data['username'] + " has left the " + data['room'] + " room."}, room=data['room'])
+
+
+@socketio.on('like')
+def like(msg):
+    # like_send(data['room'])
+    # print(f"\n\n{msg}\n\n")
+    print(f"\n\ntoto\n\n")
+    send("test")
+
+
+# @ socketio.on('create')
+# def create(data):
+# username = data['username']
+# room = data['room_name']
+# join_room(data[room])
+# send(username + ' has left the room.', room=room)
+
+
+# Lance les serveurs
+if __name__ == '__main__':
+    socketio.run(app)
